4b6c4daaeb6b21c6245108d15ff57b7f1dcb6aa8
uhppoted.dev <uhppoted@twyst.co.za>
2020-07-31 12:21:29 -0700

    Adding support for general command options (e.g. --debug, --config)

--- command/command.go
+++ command/command.go
@@ -16,6 +16,15 @@ type Command interface {
 	Help()
 }
 
+type CommandV interface {
+	Name() string
+	FlagSet() *flag.FlagSet
+	Execute(context.Context, ...interface{}) error
+	Description() string
+	Usage() string
+	Help()
+}
+
 // Can't invoke flag.Parse() because the 'run' is the default command i.e. only want to parse the flags
 // if the other commands are not being invoked
 func Parse(cli []Command, run Command, help Command) (Command, error) {
@@ -42,6 +51,37 @@ func Parse(cli []Command, run Command, help Command) (Command, error) {
 		if flagset == nil {
 			panic(fmt.Sprintf("'%s' command implementation without a flagset: %#v", cmd.Name(), cmd))
 		}
+		return cmd, flagset.Parse(args)
+	}
+
+	return nil, nil
+}
+
+func ParseV(cli []CommandV, run CommandV, help CommandV) (CommandV, error) {
+	var cmd CommandV = run
+	var args []string = flag.Args()
+
+	if len(args) > 0 {
+		if args[0] == help.Name() {
+			cmd = help
+			args = args[1:]
+		} else {
+			for _, c := range cli {
+				if args[0] == c.Name() {
+					cmd = c
+					args = args[1:]
+					break
+				}
+			}
+		}
+	}
+
+	if cmd != nil {
+		flagset := cmd.FlagSet()
+		if flagset == nil {
+			panic(fmt.Sprintf("'%s' command implementation without a flagset: %#v", cmd.Name(), cmd))
+		}
+
 		return cmd, flagset.Parse(args)
 	}
 
---
+++ command/helpv.go
@@ -0,0 +1,117 @@
+package uhppoted
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+)
+
+type HelpV struct {
+	service string
+	cli     []CommandV
+	run     CommandV
+}
+
+func NewHelpV(service string, cli []CommandV, run CommandV) *HelpV {
+	return &HelpV{
+		service: service,
+		cli:     cli,
+		run:     run,
+	}
+}
+
+func (h *HelpV) Name() string {
+	return "help"
+}
+
+func (h *HelpV) FlagSet() *flag.FlagSet {
+	return flag.NewFlagSet("help", flag.ExitOnError)
+}
+
+func (h *HelpV) Description() string {
+	return "Displays the help information"
+}
+
+func (h *HelpV) Usage() string {
+	return ""
+}
+
+func (h *HelpV) Help() {
+	fmt.Println()
+	fmt.Printf("  Usage: %s help <command>\n", h.service)
+	fmt.Println()
+	fmt.Println("  Commands:")
+	fmt.Println("    help          Displays this message")
+
+	for _, c := range h.cli {
+		fmt.Printf("    %-13s %s\n", c.FlagSet().Name(), c.Description())
+	}
+}
+
+func (h *HelpV) Execute(ctx context.Context, options ...interface{}) error {
+	if len(os.Args) > 2 {
+		if os.Args[2] == "commands" {
+			h.helpCommands()
+			return nil
+		}
+
+		if os.Args[2] == h.Name() {
+			h.Help()
+			return nil
+		}
+
+		for _, c := range h.cli {
+			if os.Args[2] == c.Name() {
+				c.Help()
+				return nil
+			}
+		}
+
+		fmt.Printf("Invalid command: %v. Type 'help commands' to get a list of supported commands\n", flag.Arg(1))
+	} else {
+		h.usage()
+	}
+
+	return nil
+}
+
+func (h *HelpV) usage() {
+	fmt.Println()
+	fmt.Printf("  Usage: %s <command> [options]\n", h.service)
+	fmt.Println()
+
+	fmt.Println("  Commands:")
+	fmt.Printf("    help          Displays this message. For help on a specific command use '%s help <command>'\n", h.service)
+
+	for _, c := range h.cli {
+		fmt.Printf("    %-13s %s\n", c.FlagSet().Name(), c.Description())
+	}
+
+	fmt.Println()
+
+	if h.run != nil {
+		fmt.Println("  Defaults to 'run'.")
+		fmt.Println()
+		fmt.Println(" 'run' options:")
+
+		h.run.FlagSet().VisitAll(func(f *flag.Flag) {
+			fmt.Printf("    --%-12s %s\n", f.Name, f.Usage)
+		})
+
+		fmt.Println()
+	}
+}
+
+func (h *HelpV) helpCommands() {
+	fmt.Println()
+	fmt.Println("  Supported commands:")
+
+	for _, c := range h.cli {
+		fmt.Printf("     %-16s %s\n", c.FlagSet().Name(), c.Usage())
+	}
+
+	fmt.Println()
+	fmt.Println("     Defaults to 'run'.")
+	fmt.Println()
+}
