01f2a4f25c212e542351d9ca1188add97b765ce0
uhppoted.dev <uhppoted@twyst.co.za>
2020-08-05 12:13:41 -0700

    Added RequireClientCertificates to configuration for 'uhppoted-httpd' and 'uhppoted-rest'

--- config/httpd.go
+++ config/httpd.go
@@ -3,27 +3,29 @@ package config
 import ()
 
 type HTTPD struct {
-	AuthDB         string `conf:"auth.local.db"`
-	CookieMaxAge   int    `conf:"cookie.max-age"`
-	SessionExpiry  string `conf:"session.expiry"`
-	HttpEnabled    bool   `conf:"http.enabled"`
-	HttpPort       uint16 `conf:"http.port"`
-	HttpsEnabled   bool   `conf:"https.enabled"`
-	HttpsPort      uint16 `conf:"https.port"`
-	CACertificate  string `conf:"tls.ca"`
-	TLSCertificate string `conf:"tls.certificate"`
-	TLSKey         string `conf:"tls.key"`
+	AuthDB                    string `conf:"auth.local.db"`
+	CookieMaxAge              int    `conf:"cookie.max-age"`
+	SessionExpiry             string `conf:"session.expiry"`
+	HttpEnabled               bool   `conf:"http.enabled"`
+	HttpPort                  uint16 `conf:"http.port"`
+	HttpsEnabled              bool   `conf:"https.enabled"`
+	HttpsPort                 uint16 `conf:"https.port"`
+	CACertificate             string `conf:"tls.ca"`
+	TLSCertificate            string `conf:"tls.certificate"`
+	TLSKey                    string `conf:"tls.key"`
+	RequireClientCertificates bool   `conf:"tls.client.certificates"`
 }
 
 func NewHTTPD() *HTTPD {
 	return &HTTPD{
-		AuthDB:         httpdAuthDB,
-		CookieMaxAge:   24,
-		SessionExpiry:  "60m",
-		HttpEnabled:    false,
-		HttpsEnabled:   true,
-		CACertificate:  httpdCACertificate,
-		TLSCertificate: httpdTLSCertificate,
-		TLSKey:         httpdTLSKey,
+		AuthDB:                    httpdAuthDB,
+		CookieMaxAge:              24,
+		SessionExpiry:             "60m",
+		HttpEnabled:               false,
+		HttpsEnabled:              true,
+		CACertificate:             httpdCACertificate,
+		TLSCertificate:            httpdTLSCertificate,
+		TLSKey:                    httpdTLSKey,
+		RequireClientCertificates: false,
 	}
 }
--- config/rest.go
+++ config/rest.go
@@ -3,18 +3,19 @@ package config
 import ()
 
 type REST struct {
-	HttpEnabled        bool   `conf:"http.enabled"`
-	HttpPort           uint16 `conf:"http.port"`
-	HttpsEnabled       bool   `conf:"https.enabled"`
-	HttpsPort          uint16 `conf:"https.port"`
-	TLSKeyFile         string `conf:"tls.key"`
-	TLSCertificateFile string `conf:"tls.certificate"`
-	CACertificateFile  string `conf:"tls.ca"`
-	CORSEnabled        bool   `conf:"CORS.enabled"`
-	AuthEnabled        bool   `conf:"auth.enabled"`
-	Users              string `conf:"auth.users"`
-	Groups             string `conf:"auth.groups"`
-	HOTP               HOTP   `conf:"auth.hotp"`
+	HttpEnabled               bool   `conf:"http.enabled"`
+	HttpPort                  uint16 `conf:"http.port"`
+	HttpsEnabled              bool   `conf:"https.enabled"`
+	HttpsPort                 uint16 `conf:"https.port"`
+	TLSKeyFile                string `conf:"tls.key"`
+	TLSCertificateFile        string `conf:"tls.certificate"`
+	CACertificateFile         string `conf:"tls.ca"`
+	RequireClientCertificates bool   `conf:"tls.client.certificates"`
+	CORSEnabled               bool   `conf:"CORS.enabled"`
+	AuthEnabled               bool   `conf:"auth.enabled"`
+	Users                     string `conf:"auth.users"`
+	Groups                    string `conf:"auth.groups"`
+	HOTP                      HOTP   `conf:"auth.hotp"`
 }
 
 type OpenAPI struct {
@@ -24,17 +25,18 @@ type OpenAPI struct {
 
 func NewREST() *REST {
 	return &REST{
-		HttpEnabled:        false,
-		HttpPort:           8080,
-		HttpsEnabled:       true,
-		HttpsPort:          8443,
-		TLSKeyFile:         "uhppoted.key",
-		TLSCertificateFile: "uhppoted.cert",
-		CACertificateFile:  "ca.cert",
-		CORSEnabled:        false,
-		AuthEnabled:        false,
-		Users:              restUsers,
-		Groups:             restGroups,
+		HttpEnabled:               false,
+		HttpPort:                  8080,
+		HttpsEnabled:              true,
+		HttpsPort:                 8443,
+		TLSKeyFile:                "uhppoted.key",
+		TLSCertificateFile:        "uhppoted.cert",
+		CACertificateFile:         "ca.cert",
+		RequireClientCertificates: true,
+		CORSEnabled:               false,
+		AuthEnabled:               false,
+		Users:                     restUsers,
+		Groups:                    restGroups,
 		HOTP: HOTP{
 			Range:    8,
 			Counters: restHOTP,
